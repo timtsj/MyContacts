1d42d5ca1411a916aa8d795bbf68eca5b74cd6f1
Timur <timur.seisembayev@gmail.com>
2019-09-04 09:49:03 +0600

    feat: data binding for adapter

--- app/src/main/java/com/tsdreamdeveloper/mycontacts/ui/adapter/ContactAdapter.java
+++ app/src/main/java/com/tsdreamdeveloper/mycontacts/ui/adapter/ContactAdapter.java
@@ -16,16 +16,16 @@
 
 package com.tsdreamdeveloper.mycontacts.ui.adapter;
 
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.TextView;
 
 import com.tsdreamdeveloper.mycontacts.R;
+import com.tsdreamdeveloper.mycontacts.databinding.ListItemContactBinding;
 import com.tsdreamdeveloper.mycontacts.mvp.model.Contact;
 
 import java.util.ArrayList;
@@ -62,15 +62,14 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     @NonNull
     @Override
     public ContactAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_dummy_data, parent, false);
-        return new ViewHolder(view);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ListItemContactBinding binding = DataBindingUtil.inflate(inflater, R.layout.list_item_contact, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactAdapter.ViewHolder holder, int position) {
-        Contact item = contactListFiltered.get(position);
-        String name = item.getName() + " " + item.getPhone();
-        holder.mNameTV.setText(name);
+        holder.bind(contactListFiltered.get(position));
     }
 
     @Override
@@ -79,11 +78,16 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        final TextView mNameTV;
+        ListItemContactBinding binding;
 
-        ViewHolder(View view) {
-            super(view);
-            mNameTV = view.findViewById(R.id.tv_name);
+        ViewHolder(ListItemContactBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bind(Contact contact) {
+            binding.setContact(contact);
+            binding.executePendingBindings();
         }
     }
 
